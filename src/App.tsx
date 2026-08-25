1fb7ae430ca5053fa27f5b5de3ed36d4a027c65d
Essence Roberts <essencenroberts@gmail.com>
2026-08-25 14:28:27 -0500

    render charactercounter in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 // import { useState } from 'react';
+import CharacterCount from './components/CharacterCounter/CharacterCounter';
 import './App.css';
 
+
 function App() {
-  return 
+  return (
+    <>
+      <h1 className='text-2xl text-center font-sans font-bold text-slate-800 m-5 '>State and Event Lab</h1>
+      
+      <CharacterCount />
+
+    </>
+    
+  )
 }
 
 export default App;
